ac886701d21ded4399399b2ccc221663fed71632
mikecebul <mikecebul@gmail.com>
2023-11-20 02:27:47 -0500

    Chore: Add Google Maps

---
+++ _components/simple-map.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React from "react";
+import GoogleMapReact from "google-map-react";
+
+// const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+export default function SimpleMap() {
+  const defaultProps = {
+    center: {
+      lat: 45.314789,
+      lng: -85.25872,
+    },
+    zoom: 11,
+  };
+
+  return (
+    // Important! Always set the container height explicitly
+    <div className="w-full h-[600px]">
+      <GoogleMapReact
+        bootstrapURLKeys={{
+          key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_SK as string,
+        }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        {/* <AnyReactComponent lat={45.314789} lng={-85.25872} text="Chx" /> */}
+      </GoogleMapReact>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import { ModeToggle } from "@/_components/mode-toggle";
-import { Button } from "@/_components/ui/button";
-import { Calendar } from "@/_components/ui/calendar";
+import SimpleMap from "@/_components/simple-map";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-16">
       <ModeToggle />
+      <SimpleMap />
     </main>
   );
 }
