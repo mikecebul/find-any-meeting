1daedc071c606901bdd295520437b591686552b9
mikecebul <mikecebul@gmail.com>
2023-11-24 15:24:21 -0500

    Chore: Use live data

--- app/page.tsx
+++ app/page.tsx
@@ -1,124 +1,25 @@
-"use client";
-
-import { Label } from "@/components/ui/label";
-import {
-  SelectValue,
-  SelectTrigger,
-  SelectItem,
-  SelectGroup,
-  SelectContent,
-  Select,
-} from "@/components/ui/select";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import {
-  DialogTrigger,
-  DialogFooter,
-  DialogContent,
-  Dialog,
-} from "@/components/ui/dialog";
 import GoogleMap from "@/components/google-map";
 
-export default function Component() {
-  return (
-    <main className="w-full h-[100svh] relative">
-      <div className="hidden lg:block absolute inset-0">
-        <GoogleMap />
-      </div>
-      <aside className="hidden lg:block absolute top-0 left-0 p-4 lg:w-64 lg:h-full bg-white shadow-lg lg:shadow-none lg:bg-transparent lg:p-0 z-10">
-        <Search />
-      </aside>
-      {/* <div className="lg:hidden py-8">
-          <Dialog>
-            <DialogTrigger className="w-full">
-              <Button>Open Search Menu</Button>
-            </DialogTrigger>
-            <DialogContent>
-              <Search />
-              <DialogFooter>
-                <Button variant="secondary">Cancel</Button>
-                <Button variant="default">Search</Button>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
-          <ul className="divide-y divide-gray-200">
-            <li className="p-4">
-              <h2 className="font-semibold">Meeting 1</h2>
-              <p className="text-sm text-gray-500">Meeting details...</p>
-            </li>
-            <li className="p-4">
-              <h2 className="font-semibold">Meeting 2</h2>
-              <p className="text-sm text-gray-500">Meeting details...</p>
-            </li>
-            <li className="p-4">
-              <h2 className="font-semibold">Meeting 3</h2>
-              <p className="text-sm text-gray-500">Meeting details...</p>
-            </li>
-          </ul>
-        </div> */}
-    </main>
+async function getData() {
+  const res = await fetch(
+    "https://fam-payload-production.up.railway.app/api/locations"
   );
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
 }
 
-function Search() {
+export default async function Component() {
+  const data = await getData();
   return (
-    <nav className="space-y-1 lg:space-y-4 bg-slate-950/90 px-4 py-8 rounded-br-md">
-      <h1 className="text-2xl font-bold">Meeting Finder</h1>
-      <p className="text-sm font-light">Find meetings near you</p>
-      <div className="space-y-2 lg:space-y-4">
-        <Label htmlFor="meeting-type">Meeting Type</Label>
-        <Select>
-          <SelectTrigger>
-            <SelectValue placeholder="Select a meeting type" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="type1">Type 1</SelectItem>
-              <SelectItem value="type2">Type 2</SelectItem>
-              <SelectItem value="type3">Type 3</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </div>
-      <div className="space-y-2 lg:space-y-4">
-        <Label htmlFor="distance">Distance</Label>
-        <Input
-          aria-describedby="distance-constraint"
-          id="distance"
-          placeholder="In miles"
-          required
-          type="number"
-        />
-        <p className="text-sm text-gray-500" id="distance-constraint">
-          Enter a number from 1 to 100
-        </p>
-      </div>
-      <div className="space-y-2 lg:space-y-4">
-        <Label htmlFor="day">Day</Label>
-        <Select>
-          <SelectTrigger>
-            <SelectValue placeholder="Select a day" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="monday">Monday</SelectItem>
-              <SelectItem value="tuesday">Tuesday</SelectItem>
-              <SelectItem value="wednesday">Wednesday</SelectItem>
-              <SelectItem value="thursday">Thursday</SelectItem>
-              <SelectItem value="friday">Friday</SelectItem>
-              <SelectItem value="saturday">Saturday</SelectItem>
-              <SelectItem value="sunday">Sunday</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </div>
-      <div className="space-y-2 lg:space-y-4">
-        <Label htmlFor="time">Time</Label>
-        <Input id="time" required type="time" />
-      </div>
-      <Button className="w-full mt-4" type="submit">
-        Search
-      </Button>
-    </nav>
+    <main className="w-full h-[100svh]">
+      <GoogleMap data={data} />
+    </main>
   );
 }
--- components/google-map.tsx
+++ components/google-map.tsx
@@ -8,30 +8,77 @@ import {
   Pin,
   InfoWindow,
 } from "@vis.gl/react-google-maps";
-import { Location, Meeting, locations } from "@/data/locations";
+import { format } from "date-fns";
 
-export default function GoogleMap() {
+export interface Root {
+  docs: Doc[];
+  totalDocs: number;
+  limit: number;
+  totalPages: number;
+  page: number;
+  pagingCounter: number;
+  hasPrevPage: boolean;
+  hasNextPage: boolean;
+  prevPage: any;
+  nextPage: any;
+}
+
+export interface Doc {
+  id: string;
+  name: string;
+  location: Location;
+  meetings: Meeting[];
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Location {
+  address: string;
+  position: number[];
+}
+
+export interface Meeting {
+  meeting: Meeting2;
+  id: string;
+}
+
+export interface Meeting2 {
+  pathway: string;
+  group: string;
+  type: string;
+  dayAndTime: DayAndTime;
+}
+
+export interface DayAndTime {
+  isRecurring: boolean;
+  dayOfWeek: string;
+  timeOnly: string;
+}
+
+export default function GoogleMap({ data }: { data: Root }) {
   const position = {
     lat: 45.3156822,
     lng: -85.2600135,
   };
-  const [selectedLocation, setSelectedLocation] = useState<Location | null>(
-    null
-  );
+  const [selectedLocation, setSelectedLocation] = useState<Doc | null>(null);
 
-  const handleMarkerClick = (location: Location) => {
-    setSelectedLocation(location);
+  const handleMarkerClick = (doc: Doc) => {
+    setSelectedLocation(doc);
   };
+
+  function convertPositionToArray(position: number[]) {
+    return { lat: position[1], lng: position[0] };
+  }
 
   return (
     <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}>
       <div className="w-full h-[100svh]">
         <Map zoom={9} center={position} mapId={process.env.NEXT_PUBLIC_MAP_ID}>
-          {locations.map((location, index) => (
+          {data.docs.map((doc, index) => (
             <AdvancedMarker
               key={index}
-              position={location.position}
-              onClick={() => handleMarkerClick(location)}
+              position={convertPositionToArray(doc.location.position)}
+              onClick={() => handleMarkerClick(doc)}
             >
               <Pin
               // background={"grey"}
@@ -42,7 +89,9 @@ export default function GoogleMap() {
           ))}
           {!!selectedLocation && (
             <InfoWindow
-              position={position}
+              position={convertPositionToArray(
+                selectedLocation.location.position
+              )}
               onCloseClick={() => setSelectedLocation(null)}
             >
               <div>
@@ -50,15 +99,23 @@ export default function GoogleMap() {
                   {selectedLocation.name}
                 </p>
                 <p className="text-black text-base">
-                  {selectedLocation.address}
+                  {selectedLocation.location.address}
                 </p>
                 <div className="w-full h-1 bg-slate-950 mb-2 rounded-full"></div>
                 <ul>
                   {selectedLocation.meetings
-                    .filter((meeting) => meeting.type !== "Zoom")
-                    .map((meeting, index) => (
+                    .filter(({ meeting }) => meeting.type !== "Zoom")
+                    .map(({ meeting }, index) => (
                       <li key={index} className="text-black text-base">
-                        {meeting.day} at {meeting.time} - {meeting.group}
+                        <span className="capitalize">
+                          {meeting.dayAndTime.dayOfWeek}{" "}
+                        </span>
+                        at{" "}
+                        {format(
+                          new Date(meeting.dayAndTime.timeOnly),
+                          "h:mm a"
+                        )}{" "}
+                        - <span className="uppercase">{meeting.pathway}</span>
                       </li>
                     ))}
                 </ul>
