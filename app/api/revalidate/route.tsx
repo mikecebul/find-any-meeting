049321f39152f3def3602fb1e51323fd40f22de3
mikecebul <mikecebul@gmail.com>
2023-11-25 02:01:04 -0500

    Test: Switch revalidate to POST req

--- app/api/revalidate/route.tsx
+++ app/api/revalidate/route.tsx
@@ -1,22 +1,23 @@
 import { NextRequest } from "next/server";
 import { revalidatePath } from "next/cache";
 
-export async function GET(request: NextRequest) {
-  const secret = request.nextUrl.searchParams.get("secret");
-  const path = request.nextUrl.searchParams.get("path");
+export async function POST(request: NextRequest) {
+  // const secret = request.nextUrl.searchParams.get("secret");
+  // const path = request.nextUrl.searchParams.get("path");
+  const { secret, path } = await request.json();
 
   // Check if the provided secret matches the one in the environment variables
   if (secret !== process.env.REVALIDATION_SECRET) {
     return new Response("Invalid secret", { status: 401 });
   }
   if (path) {
-    revalidatePath(path)
-    return Response.json({ revalidated: true, path: path, now: Date.now() })
+    revalidatePath(path);
+    return Response.json({ revalidated: true, path: path, now: Date.now() });
   }
- 
+
   return Response.json({
     revalidated: false,
     now: Date.now(),
-    message: 'Missing path to revalidate',
-  })
+    message: "Missing path to revalidate",
+  });
 }
