abddb64adb40b1581cdf96668c4cdcb2032e2092
mikecebul <mikecebul@gmail.com>
2023-11-25 12:32:10 -0500

    Test: Revalidate with path and secret on canary

--- app/api/revalidate/route.tsx
+++ app/api/revalidate/route.tsx
@@ -2,6 +2,20 @@ import { NextRequest } from "next/server";
 import { revalidatePath } from "next/cache";
 
 export async function GET(request: NextRequest) {
-  revalidatePath("/");
-  return Response.json({ revalidated: true, path: "/", now: Date.now() });
+  const secret = request.nextUrl.searchParams.get("secret");
+  const path = request.nextUrl.searchParams.get("path");
+
+  if (secret !== process.env.REVALIDATION_SECRET) {
+    return new Response("Invalid secret", { status: 401 });
+  }
+  if (path) {
+    revalidatePath(path);
+    return Response.json({ revalidated: true, path: path, now: Date.now() });
+  }
+
+  return Response.json({
+    revalidated: false,
+    now: Date.now(),
+    message: "Missing path to revalidate",
+  });
 }
