d216156bab667ca14768a3c8a120f0727405006b
mikecebul <mikecebul@gmail.com>
2023-11-25 00:50:04 -0500

    Fix: update revalidate route for Path

--- app/api/revalidate/route.tsx
+++ app/api/revalidate/route.tsx
@@ -3,14 +3,20 @@ import { revalidatePath } from "next/cache";
 
 export async function GET(request: NextRequest) {
   const secret = request.nextUrl.searchParams.get("secret");
+  const path = request.nextUrl.searchParams.get("path");
 
   // Check if the provided secret matches the one in the environment variables
   if (secret !== process.env.REVALIDATION_SECRET) {
     return new Response("Invalid secret", { status: 401 });
   }
-  revalidatePath("/");
-  return new Response(JSON.stringify({ revalidated: true, now: Date.now() }), {
-    headers: { "Content-Type": "application/json" },
-    status: 200,
-  });
+  if (path) {
+    revalidatePath(path)
+    return Response.json({ revalidated: true, now: Date.now() })
+  }
+ 
+  return Response.json({
+    revalidated: false,
+    now: Date.now(),
+    message: 'Missing path to revalidate',
+  })
 }
